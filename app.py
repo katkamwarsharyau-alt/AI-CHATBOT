a619be80a3df8e46b7a0b6440f01957f5c38f92f
katkamwarsharyau-alt <katkamwarsharyau@gmail.com>
2026-04-18 04:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+   import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
